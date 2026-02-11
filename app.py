f7e410f4759780ffd2e343d6103b7fcbb821350d
ARAVI9047 <60138555+ARAVI9047@users.noreply.github.com>
2026-02-11 13:19:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
     return "Bot is running!"
 
 def run_bot():
-    import wingo_MLPrediction  # just import your bot file
+    import wingo_MLprediction_bot  # just import your bot file
 
 if __name__ == "__main__":
     # Start bot in background thread
@@ -18,3 +18,4 @@ if __name__ == "__main__":
     # Start web server (required for Render free)
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
